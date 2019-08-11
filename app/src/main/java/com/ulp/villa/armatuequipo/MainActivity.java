b74c63238e0c5e303058daf0add4f18c2a8a45b2
dvillarreal1234 <villarrealdamian@hotmail.com.ar>
2019-08-10 21:36:16 -0300

    Hice la autenticación con 4 formas diferentes...la primera con cualquier correo, lo bueno que autentica si es verdadero o no el correo con la contraseña; la segunda con el telefono, te manda un SMS pero no se porque no se manda (hay que ver porque pasa eso si queremos esa forma); la tercera es por Facebook pero no anda, tengo que arreglarlo..en estos dias lo voy hacer; y la última es por una cuenta de Google, que esta anda correcta. Modifique el build.gradle, agregué un archivo en app llamado google-services.json...este se agrega para realizar la autenticación con Firebase, modifiqué el AndroidManifest.xml, también el content_main.xml ubicado en res/layout/ y los archivos strings.xml y styles.xml de res/values/

--- app/src/main/java/com/ulp/villa/armatuequipo/MainActivity.java
+++ app/src/main/java/com/ulp/villa/armatuequipo/MainActivity.java
@@ -1,18 +1,33 @@
 package com.ulp.villa.armatuequipo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -23,8 +38,14 @@ import android.view.Menu;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int MY_REQUEST_CODE = 7117;// Any number you want
+    List<AuthUI.IdpConfig> providers;
+    Button btn_sign_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,46 +69,49 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
-        Button btnFace = (Button) findViewById(R.id.btnFacebook);
-        Button btnGoogle = (Button) findViewById(R.id.btnGoogle);
-        Button btnAyuda = (Button) findViewById(R.id.btnAyuda);
-        Button btnSobreNos = (Button) findViewById(R.id.btnSobreNosotros);
-        Button btnInvitado = (Button) findViewById(R.id.btnInvitado);
 
-        btnFace.setOnClickListener(new View.OnClickListener() {
+        btn_sign_out = (Button) findViewById(R.id.btn_sign_out);
+        btn_sign_out.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Login con Face", Toast.LENGTH_SHORT).show();
+                //Logout
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                btn_sign_out.setEnabled(false);
+                                showSignInOptions();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
-        btnGoogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Login con Google", Toast.LENGTH_SHORT).show();
-            }
-        });
 
-        btnAyuda.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Aiuda", Toast.LENGTH_SHORT).show();
-            }
-        });
+        //Init provider
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(), //Email Builder
+                new AuthUI.IdpConfig.PhoneBuilder().build(), //Phone Builder
+                new AuthUI.IdpConfig.FacebookBuilder().build(), //Facebook Builder
+                new AuthUI.IdpConfig.GoogleBuilder().build() //Google Builder
+        );
 
-        btnSobreNos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Sobre nosotros", Toast.LENGTH_SHORT).show();
-            }
-        });
+        showSignInOptions();
 
-        btnInvitado.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Ingresar como invitado", Toast.LENGTH_SHORT).show();
-            }
-        });
+    }
+
+    private void showSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTheme(R.style.MyTheme)
+                .build(),MY_REQUEST_CODE
+        );
     }
 
     @Override
@@ -145,5 +169,27 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == MY_REQUEST_CODE)
+        {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode == RESULT_OK)
+            {
+                //Get User
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                //Show email on Toast
+                Toast.makeText(this,""+user.getEmail(), Toast.LENGTH_SHORT).show();
+                //Set Button signout
+                btn_sign_out.setEnabled(true);
+            }
+            else
+            {
+                Toast.makeText(this,""+response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
